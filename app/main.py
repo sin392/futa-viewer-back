7e648cbdaff216eb10650cc289750883d661d64a
sin392 <sin392@gmail.com>
2022-01-24 19:30:57 +0900

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from routers.catalog import router as catalog_router
 from routers.threads import router as threads_router
 
 app = FastAPI()
 
-app.include_router(catalog_router, prefix='/catalog')
-app.include_router(threads_router, prefix='/threads')
-
 
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
+
+router = APIRouter()
+router.include_router(catalog_router, prefix='/catalog')
+router.include_router(threads_router, prefix='/threads')
+
+app.include_router(router, prefix='v1')
